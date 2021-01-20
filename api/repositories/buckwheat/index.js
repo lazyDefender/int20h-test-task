20c0e1efcdc9de627ba4e65a6a06ca78e4667472
lazyDefender <lazyDefender@users.noreply.github.com>
2021-01-20 09:11:49 +0200

    comments

--- api/repositories/buckwheat/index.js
+++ api/repositories/buckwheat/index.js
@@ -10,25 +10,34 @@ const repo = {
             const body = root.childNodes[1].childNodes[2]
             const goodsTiles = body.querySelectorAll('.goods-tile')
             for(let tile of goodsTiles) {
+                // Availability
                 const availability = tile
                     .querySelector('.goods-tile__availability')
                     .text
                     .trim()
 
+                // if item is not available there is no need to continue parsing
                 if(availability === 'Є в наявності') {
                     const picture = tile
                     .querySelector('.goods-tile__picture')
+
+                    // url
                     const url = picture.getAttribute('href')
+
+                    // imgSrc
                     const imgSrc = picture
                         .querySelectorAll('img')
                         .pop()
                         .getAttribute('src')
                         .trim()
+                    
+                    // title
                     const title = tile
                         .querySelector('.goods-tile__title')
                         .text
                         .trim()
 
+                    // price
                     const priceRaw = tile
                         .querySelector('.goods-tile__prices')
                         .lastChild
@@ -38,8 +47,10 @@ const repo = {
                     const priceWithoutHrn = priceRaw
                         .substring(0, indexOfHrn)
                         .trim()
+                    // '2 020' -> '2020'
                     const priceWithoutSpace = priceWithoutHrn.replace(/\s/, '')
                     const price = Number(priceWithoutSpace)
+
 
                     foundItems.push({
                         url, 
@@ -48,7 +59,6 @@ const repo = {
                         price,
                     })
                 }
-                
             }
         }
         return foundItems
@@ -63,23 +73,32 @@ const repo = {
             const listItems = body.querySelectorAll('.products-box__list-item')
             for(let item of listItems) {
                 const productTile = item.querySelector('a.product-tile')
+
+                // url
                 const url = `${rootUrl}${productTile.getAttribute('href')}`
+
+                // imgSrc
                 const imgSrc = item
                     .querySelector('.product-tile__image-i')
                     .getAttribute('src')
 
+                // price
                 const priceRaw = item
                     .querySelector('.Price__value_caption')
                     .text
                 const price = Number(priceRaw)
 
+                // title
                 const title = item
                     .querySelector('.product-tile__title')
                     .text
 
+                // weight 
                 const weightRaw = item
                     .querySelector('.product-tile__weight')
-                    .text
+                    .text   
+                // '800 г' -> 800
+                // '1 кг' -> 1000
                 const weightParts = weightRaw.split(' ')
                 const weight = weightParts[1] === 'г' ? 
                     Number(weightParts[0]) :
@@ -106,23 +125,32 @@ const repo = {
             const listItems = body.querySelectorAll('.products-box__list-item')
             for(let item of listItems) {
                 const productTile = item.querySelector('a.product-tile')
+
+                // url
                 const url = `${rootUrl}${productTile.getAttribute('href')}`
+
+                // imgSrc
                 const imgSrc = item
                     .querySelector('.product-tile__image-i')
                     .getAttribute('src')
 
+                // price
                 const priceRaw = item
                     .querySelector('.Price__value_caption')
                     .text
                 const price = Number(priceRaw)
 
+                // title
                 const title = item
                     .querySelector('.product-tile__title')
                     .text
 
+                // weight
                 const weightRaw = item
                     .querySelector('.product-tile__weight')
                     .text
+                // '800 г' -> 800
+                // '1 кг' -> 1000
                 const weightParts = weightRaw.split(' ')
                 const weight = weightParts[1] === 'г' ? 
                     Number(weightParts[0]) :
@@ -149,18 +177,26 @@ const repo = {
             const listItems = body.querySelectorAll('.card__info')
             for(let item of listItems) {
                 const cardPhoto = item.querySelector('.card__photo')
-                const productUrl = cardPhoto.getAttribute('href')
+
+                // url
+                const productUrl = cardPhoto.getAttribute('href') 
                 const url = `${rootUrl}${productUrl}`
+
+                // imgSrc
                 const imgSrc = cardPhoto
                     .querySelector('img')
                     .getAttribute('src')
                     .trim()
+                
+                // title
                 const title = item
                     .querySelector('.card__name')
                     .text
                     .replace(/\n/g, '')
                     .trim()
-
+                
+                // price
+                // '30грн' -> 30
                 const priceRaw = item
                     .querySelector('.card__price')
                     .text
@@ -169,7 +205,9 @@ const repo = {
                 const priceEndIndex = priceRaw.indexOf('г')
                 const price = Number(priceRaw.substring(0, priceEndIndex))
 
+                // if available
                 if(price > 0) {
+                    // weight
                     const weightRaw = item
                         .querySelector('.card__characteristics')
                         .childNodes[5]
--- api/routes/buckwheat/index.js
+++ api/routes/buckwheat/index.js
@@ -5,16 +5,36 @@ const router = express.Router()
 const buckwheatRepository = require('../../repositories/buckwheat')
 
 router.get('/', async (req, res) => {
+    const {
+        minPrice,
+        maxPrice,
+        weights,
+    } = req.query
+
     const rozetkaItems = await buckwheatRepository.getFromRozetka()
     const metroItems = await buckwheatRepository.getFromMetro()
     const novusItems = await buckwheatRepository.getFromNovus()
     const epicentrItems = await buckwheatRepository.getFromEpicentr()
-    const allItems = [
+
+    let allItems = [
         ...rozetkaItems,
         ...metroItems,
         ...novusItems,
         ...epicentrItems,
     ]
+    
+    // filter by price
+    if(minPrice && maxPrice) {
+        const min = parseFloat(minPrice)
+        const max = parseFloat(maxPrice)
+        allItems = allItems.filter(i => i.price >= min && i.price <= max)
+    }
+
+    // filter by weight 
+    if(weights) {
+        const weightsList = weights.split(',').map(w => parseInt(w))
+        allItems = allItems.filter(i => weightsList.includes(i.weight))
+    }
     res.json(allItems)
 })
 
