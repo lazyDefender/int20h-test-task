5366a64e95e7b3ef61d46bf56b8bea457c188159
lazyDefender <lazyDefender@users.noreply.github.com>
2021-01-20 18:52:19 +0200

    init

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { history } from './navigation/history'
+import { Routes } from './navigation'
 import './App.css'
 
 const App = () => {
   return (
-    <></>
+    <Router history={history}>
+      <Routes/>
+    </Router>
   )
 }
 
---
+++ client/src/bus/root/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Root = () => {
+    return (
+        <div>
+            <button>Оновити</button>
+            <select>
+                <option value="">За зростанням ціни</option>
+                <option value="">За спаданням ціни</option>
+            </select>
+            <div>
+                <article>
+                    <img src="" alt=""/>
+                    <h3>Гречка</h3>
+                    <span>24 грн</span>
+                    <span>500 г</span>
+                    <a>Переглянути на Rozetka</a>
+                </article>
+            </div>
+        </div>
+    )
+}
+
+export default Root
