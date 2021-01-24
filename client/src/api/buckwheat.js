853bcaaa376d6cdc0ae943409bb99b1c7a61f189
lazyDefender <lazyDefender@users.noreply.github.com>
2021-01-24 18:32:14 +0200

    added search params

--- client/src/api/buckwheat.js
+++ client/src/api/buckwheat.js
@@ -8,11 +8,15 @@ export const api = {
             weights,
             priceOrder,
         } = query
+        
         const searchParams = new URLSearchParams()
-        // searchParams.set('minPrice', minPrice)
-        // searchParams.set('maxPrice', maxPrice)
-        // searchParams.set('weights', weights.join(','))
-        // searchParams.set('priceOrder', priceOrder)
+        if(minPrice && maxPrice && weights && priceOrder) {
+            searchParams.set('minPrice', minPrice)
+            searchParams.set('maxPrice', maxPrice)
+            searchParams.set('weights', weights.join(','))
+            searchParams.set('priceOrder', priceOrder)
+        }
+        
         const searchStr = searchParams.toString()
         const response = await axios.get(`${apiURL}/buckwheat?${searchStr}`)
         return response
